8af158f8fbc081c19aa1700eea9a61b705eae74e
Chandu B <32214321+ChandraVardhan97@users.noreply.github.com>
2024-06-20 19:05:53 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -7,9 +7,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "kkloud1234"
-    dynamodb_table = "statelock"
     key = "global/mystatefile/terraform.tfstate"
-    encrypt = true
     region = "us-east-1"
   }
 }
