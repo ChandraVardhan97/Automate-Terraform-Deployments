c76f5c8ec9a7889f45bbe98f2d36f0718409fd5e
Chandu B <32214321+ChandraVardhan97@users.noreply.github.com>
2024-06-20 19:59:04 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -84,7 +84,7 @@ resource "aws_key_pair" "my_key" {
 # Create an EC2 instance within the VPC
 resource "aws_instance" "my_ec2" {
   ami                    = "ami-0149b2da6ceec4bb0" # Replace with your desired AMI
-  instance_type          = "t2.micro"
+  instance_type          = "t2.medium"
   subnet_id              = aws_subnet.my_subnet.id
   vpc_security_group_ids = [aws_security_group.my_sg.id]
   associate_public_ip_address = true # Associate a public IP address
